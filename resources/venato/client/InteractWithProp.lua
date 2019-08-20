e052bb0e723baa4fdf0a0c278c613a9d83304ab6
crocrodail <teamcrocro@gmail.com>
2019-08-20 22:28:25 +0200

    Update InteractWithProp.lua

--- resources/venato/client/InteractWithProp.lua
+++ resources/venato/client/InteractWithProp.lua
@@ -32,9 +32,15 @@ Citizen.CreateThread(function()
 			local coordProp = GetEntityCoords(Prop, true)
 			Venato.Text3D(coordProp.x, coordProp.y, coordProp.z, "Appuyez sur ~g~E "..PropData[NextTo].text)
 			if IsControlJustPressed(1, Keys['INPUT_CONTEXT']) and GetLastInputMethod(2) then
-				AttachEntityToEntity(Venato.GetPlayerPed(), Prop, nil, PropData[NextTo].x, PropData[NextTo].y, PropData[NextTo].z, 0.0, 0.0, PropData[NextTo].h, false, false, false, false, 2, true)
-				if PropData[NextTo].anim ~= false then
-					Venato.playAnim({lib = PropData[NextTo].anim.lib, anim = PropData[NextTo].anim.anim, useLib = true, flag = PropData[NextTo].anim.flag})
+				if not AttachOnProp then
+					AttachEntityToEntity(Venato.GetPlayerPed(), Prop, nil, PropData[NextTo].x, PropData[NextTo].y, PropData[NextTo].z, 0.0, 0.0, PropData[NextTo].h, false, false, false, false, 2, true)
+					if PropData[NextTo].anim ~= false then
+						Venato.playAnim({lib = PropData[NextTo].anim.lib, anim = PropData[NextTo].anim.anim, useLib = true, flag = PropData[NextTo].anim.flag})
+						AttachOnProp = true
+					end
+				else
+					DetachEntity(Venato.GetPlayerPed())
+					ClearPedTasks(GetPlayerPed(-1))
 				end
 			end
 		end
