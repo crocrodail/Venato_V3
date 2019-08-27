26c227bfbded3506d12ee2438fa0b35ffd205dee
crocrodail <teamcrocro@gmail.com>
2019-08-27 02:59:30 +0200

    Update jobs.lua

--- resources/venato/server/jobs.lua
+++ resources/venato/server/jobs.lua
@@ -4,23 +4,23 @@ AddEventHandler('vigneron:serverRequest', function (typeRequest)
 	local player = DataPlayers[mysource].SteamId
 
 		if typeRequest == "GetRaisin" then
-			MySQL.Async.fetchScalar("SELECT quantity FROM user_inventory WHERE item_id="..vigneron_ressourceBase.." AND identifier=@identifier", {['@identifier'] = player},
+			MySQL.Async.fetchScalar("SELECT quantity FROM user_inventory WHERE item_id=26 AND identifier=@identifier", {['@identifier'] = player},
 				function(qte)
 					TriggerClientEvent('vigneron:drawGetRaisin', mysource, qte)
 				end)
 
 		elseif typeRequest == "GetVin" then
-			MySQL.Async.fetchScalar("SELECT quantity FROM user_inventory WHERE item_id="..vigneron_ressourceBase.." AND identifier=@identifier", {['@identifier'] = player},
+			MySQL.Async.fetchScalar("SELECT quantity FROM user_inventory WHERE item_id=26 AND identifier=@identifier", {['@identifier'] = player},
 				function(qteVign)
 
-					MySQL.Async.fetchScalar("SELECT quantity FROM user_inventory WHERE item_id="..vigneron_ressourceTraite.." AND identifier=@identifier", {['@identifier'] = player},
+					MySQL.Async.fetchScalar("SELECT quantity FROM user_inventory WHERE item_id=27 AND identifier=@identifier", {['@identifier'] = player},
 						function(qteTraite)
 							TriggerClientEvent('vigneron:drawGetVin',mysource, qteVign, qteTraite)
 						end)
 				end)
 
 		elseif typeRequest == "SellVin" then
-			MySQL.Async.fetchScalar("SELECT quantity FROM user_inventory WHERE item_id="..vigneron_ressourceTraite.." AND identifier=@identifier", {['@identifier'] = player},
+			MySQL.Async.fetchScalar("SELECT quantity FROM user_inventory WHERE item_id=27 AND identifier=@identifier", {['@identifier'] = player},
 				function(qte)
 					TriggerClientEvent('vigneron:drawSellVin', mysource, qte)
 				end)
@@ -69,22 +69,22 @@ AddEventHandler('brasseur:serverRequest', function (typeRequest)
 	local player = DataPlayers[mysource].SteamId
 
 	if typeRequest == "GetOrge" then
-		MySQL.Async.fetchScalar("SELECT quantity FROM user_inventory WHERE item_id="..brasseur_ressourceBase.." AND identifier=@identifier", {['@identifier'] = player},
+		MySQL.Async.fetchScalar("SELECT quantity FROM user_inventory WHERE item_id=24 AND identifier=@identifier", {['@identifier'] = player},
 			function(qte)
 				TriggerClientEvent('brasseur:drawGetOrge', mysource, qte)
 			end)
 
 	elseif typeRequest == "GetBiere" then
-		MySQL.Async.fetchScalar("SELECT quantity FROM user_inventory WHERE item_id="..brasseur_ressourceBase.." AND identifier=@identifier", {['@identifier'] = player},
+		MySQL.Async.fetchScalar("SELECT quantity FROM user_inventory WHERE item_id=24 AND identifier=@identifier", {['@identifier'] = player},
 			function(resultOrge)
-				MySQL.Async.fetchScalar("SELECT quantity FROM user_inventory WHERE item_id="..brasseur_ressourceTraite.." AND identifier=@identifier", {['@identifier'] = player},
+				MySQL.Async.fetchScalar("SELECT quantity FROM user_inventory WHERE item_id=25 AND identifier=@identifier", {['@identifier'] = player},
 					function(resultBiere)
 						TriggerClientEvent('brasseur:drawGetBiere',mysource, resultOrge, resultBiere)
 					end)
 			end)
 
 	elseif typeRequest == "SellBiere" then
-		MySQL.Async.fetchScalar("SELECT quantity FROM user_inventory WHERE item_id="..brasseur_ressourceTraite.." AND identifier=@identifier", {['@identifier'] = player},
+		MySQL.Async.fetchScalar("SELECT quantity FROM user_inventory WHERE item_id=25 AND identifier=@identifier", {['@identifier'] = player},
 			function(resultOrge)
 				TriggerClientEvent('brasseur:drawSellBiere', mysource, resultOrge)
 			end)
