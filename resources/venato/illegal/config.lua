9b2e0350f3ef029255d2d4a4171dc3ce88899ed9
Mathieu <mbusolin@digikare.com>
2019-09-06 03:29:03 +0200

    New Illegal points

--- resources/venato/illegal/config.lua
+++ resources/venato/illegal/config.lua
@@ -1,9 +1,16 @@
 drugs = { 
-    { 
-      id = 1,
-      title = 'la weed', 
-      farm = { x= 649.368, y= 100.203, z= 79.75, h= 100.401, ped_model="a_m_y_skater_01", item_id = 29, give_qte = 5, give_message = " feuille de canabis" },
-      transform = { x= -1121.773, y= -1027.69, z= 1.160, h= 22.384, ped_model="s_m_y_factory_01", origin_item_id=29, product_item_id=8, transform_qte = 5, transform_message = " joints de canabis" },
-      sell = { x= 208.272, y= -653.059, z= 42.1, h= 123.18, ped_model="s_m_y_valet_01", item_id=8, sell_qte = 5, sell_price = 100 }
-    }
+  { 
+    id = 1,
+    title = 'la weed', 
+    farm = { x= 649.368, y= 100.203, z= 79.75, h= 100.401, ped_model="a_m_y_skater_01", item_id = 29, give_qte = 5, give_message = " feuilles de canabis" },
+    transform = { x= -1121.773, y= -1027.69, z= 1.160, h= 22.384, ped_model="s_m_y_factory_01", origin_item_id=29, product_item_id=8, transform_qte = 5, transform_message = " joints de canabis" },
+    sell = { x= 208.272, y= -653.059, z= 42.1, h= 123.18, ped_model="s_m_y_valet_01", item_id=8, sell_qte = 5, sell_price = 100 }
+  },  
+  { 
+    id = 2,
+    title = 'la coke', 
+    farm = { x= 1075.772, y= -680.708, z= 56.702, h= 196.959, ped_model="u_m_m_partytarget", item_id = 32, give_qte = 5, give_message = " feuilles de coca" },
+    transform = { x= -3069.371, y= 453.741, z= 8.7, h= 63.2, ped_model="ig_chef", origin_item_id=32, product_item_id=33, transform_qte = 5, transform_message = " pochons de cocaïne" },
+    sell = { x= -1183.908, y= -518.696, z= 38.531, h= 290.374, ped_model="cs_movpremf_01", item_id=33, sell_qte = 5, sell_price = 250 }
+  }
 }
