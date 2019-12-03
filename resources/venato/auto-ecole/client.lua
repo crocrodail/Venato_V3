00177821b75559c402dd3809880fea7d5d15ddd4
SOulReaver1 <57950180+SOulReaver1@users.noreply.github.com>
2019-12-03 01:37:06 +0100

    AutoEcoleAddMENU1

--- resources/venato/auto-ecole/client.lua
+++ resources/venato/auto-ecole/client.lua
@@ -22,4 +22,34 @@ end
 Citizen.CreateThread(function()
     Citizen.Wait(10000) 
     GenerateNpc(config)
+    while true do
+        Citizen.Wait(0)
+        local x,y,z = table.unpack(GetEntityCoords(Venato.GetPlayerPed(), true)) -- Permet de recuperer la position du joueur
+        if Vdist(x, y, z, config.x, config.y, config.z) < 2 then
+            Venato.InteractTxt('Appuyez sur ~INPUT_CONTEXT~ pour ouvrir le menu')
+            if IsControlJustPressed(1, Keys['INPUT_CONTEXT']) and GetLastInputMethod(2) then
+                openmenuautoecole()
+            end
+        end
+    end  
+
 end)
+
+function openmenuautoecole()
+    Menu.clearMenu()
+    Menu.open()
+    TriggerEvent('Menu:Init', "", "La vitamine c mais ne dira rien", "#F9A82599", "https://cdn.discordapp.com/attachments/618546482135433226/618546497373339653/20181216224111_1.jpg")
+    Menu.addButton("<span class='red--text'>Fermer</span>", "closemenuautoecole")
+    Menu.addButton2("Code de la route", "openmenucode", nil, '', 'https://img.icons8.com/office/30/000000/traffic-jam.png')
+    Menu.CreateMenu()
+end
+
+function openmenucode()
+end
+
+function openmenupermis()
+end
+
+function closemenuautoecole()
+    Menu.close()
+end
