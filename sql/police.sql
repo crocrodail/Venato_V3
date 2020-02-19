98198ad75b2a7e8840d22f25f1044102cdf8da14
crocrodail <31371620+crocrodail@users.noreply.github.com>
2020-02-19 15:55:52 +0100

    update structure

--- sql/police.sql
+++
@@ -1,8 +0,0 @@
--- Add police
-
-SET @SteamId = 'steam:110000136b376ec';
-
-INSERT INTO police(`identifier`, `rank`) VALUES (@SteamId, 'Cadet');
-INSERT INTO coffres_whitelist(`CoffreId`, `UserId`) VALUES (20, @SteamId);
-INSERT INTO coffres_whitelist(`CoffreId`, `UserId`) VALUES (21, @SteamId);
-UPDATE users SET job = 2 WHERE identifier = @SteamId
